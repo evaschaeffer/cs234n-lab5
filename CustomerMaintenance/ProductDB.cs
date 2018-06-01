121821955ff064b5b1815f8e28f592f7c4875af6
Schaeffer <Eva.Schaeffer@MerrillCorp.com>
2018-05-31 18:15:23 -0700

    creating a git hub for lab 5

---
+++ CustomerMaintenance/ProductDB.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CustomerMaintenance
+{
+    public static class ProductDB
+    {
+        public static Product GetProduct(string productCode)
+        {
+            SqlConnection connection = MMABooksDB.GetConnection();
+            string selectStatement
+                = "SELECT ProductCode, Description, UnitPrice, OnHandQuantity "
+                + "FROM Products "
+                + "WHERE ProductCode = @ProductCode";
+            SqlCommand selectCommand =
+                new SqlCommand(selectStatement, connection);
+            selectCommand.Parameters.AddWithValue("@ProductCode", productCode);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader productReader =
+                    selectCommand.ExecuteReader(CommandBehavior.SingleRow);
+                if (productReader.Read())
+                {
+                    Product product = new Product();
+                    product.ProductCode = (string)productReader["ProductCode"].ToString();
+                    product.Description = productReader["Description"].ToString();
+                    product.UnitPrice = (decimal)productReader["UnitPrice"];
+                    product.OnHandQuantity = (int)productReader["OnHandQuantity"];
+                    return product;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public static bool UpdateProduct(Product oldProduct,
+        Product newProduct)
+        {
+            SqlConnection connection = MMABooksDB.GetConnection();
+            string updateStatement =
+                "UPDATE Products SET " +
+                "ProductCode = @NewProductCode, " +
+                "Description = @NewDescription, " +
+                "UnitPrice = @NewUnitPrice, " +
+                "OnHandQuantity = @NewOnHandQuantity " +
+                "WHERE ProductCode = @OldProductCode " +
+                "AND Description = @OldDescription " +
+                "AND UnitPrice = @OldUnitPrice " +
+                "AND OnHandQuantity = @OldOnHandQuantity ";
+            SqlCommand updateCommand =
+                new SqlCommand(updateStatement, connection);
+            updateCommand.Parameters.AddWithValue("@NewProductCode", newProduct.ProductCode);
+            updateCommand.Parameters.AddWithValue("@NewDescription", newProduct.Description);
+            updateCommand.Parameters.AddWithValue("@NewUnitPrice", newProduct.UnitPrice);
+            updateCommand.Parameters.AddWithValue("@NewOnHandQuantity", newProduct.OnHandQuantity);
+            updateCommand.Parameters.AddWithValue("@OldProductCode", oldProduct.ProductCode);
+            updateCommand.Parameters.AddWithValue("@OldDescription", oldProduct.Description);
+            updateCommand.Parameters.AddWithValue("@OldUnitPrice", oldProduct.UnitPrice);
+            updateCommand.Parameters.AddWithValue("@OldOnHandQuantity", oldProduct.OnHandQuantity);
+            try
+            {
+                connection.Open();
+                int count = updateCommand.ExecuteNonQuery();
+                if (count > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public static int AddProduct(Product product)
+        {
+            SqlConnection connection = MMABooksDB.GetConnection();
+            string insertStatement =
+                "INSERT Products " +
+                "(ProductCode, Description, UnitPrice, OnHandQuantity) " +
+                "VALUES (@ProductCode, @Description, @UnitPrice, @OnHandQuantity)";
+            SqlCommand insertCommand =
+                new SqlCommand(insertStatement, connection);
+            insertCommand.Parameters.AddWithValue(
+                "@ProductCode", product.ProductCode);
+            insertCommand.Parameters.AddWithValue(
+                "@Description", product.Description);
+            insertCommand.Parameters.AddWithValue(
+                "@UnitPrice", product.UnitPrice);
+            insertCommand.Parameters.AddWithValue(
+                "@OnHandQuantity", product.OnHandQuantity);
+            
+            try
+            {
+                connection.Open();
+                insertCommand.ExecuteNonQuery();
+                return 1;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public static bool DeleteProduct(Product product)
+        {
+            SqlConnection connection = MMABooksDB.GetConnection();
+            string deleteStatement =
+                "DELETE FROM Products " +
+                "WHERE ProductCode = @ProductCode " +
+                "AND Description = @Description " +
+                "AND UnitPrice = @UnitPrice " +
+                "AND OnHandQuantity = @OnHandQuantity ";
+            SqlCommand deleteCommand =
+                new SqlCommand(deleteStatement, connection);
+            deleteCommand.Parameters.AddWithValue(
+                "@ProductCode", product.ProductCode);
+            deleteCommand.Parameters.AddWithValue(
+                "@Description", product.Description);
+            deleteCommand.Parameters.AddWithValue(
+                "@UnitPrice", product.UnitPrice);
+            deleteCommand.Parameters.AddWithValue(
+                "@OnHandQuantity", product.OnHandQuantity);
+            try
+            {
+                connection.Open();
+                int count = deleteCommand.ExecuteNonQuery();
+                if (count > 0)
+                    return true;
+                else
+                    return false;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+        
+}
+
---
+++ CustomerMaintenance/ProductDBTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;  // used for testing nUnit
+
+namespace CustomerMaintenance
+{
+    [TestFixture]
+    public class ProductDBTest
+    {
+        //string productCode;
+        //string description;
+        //decimal unitPrice;
+        //int onHandQuantity;
+        Product p1;
+        Product p2;
+
+        [SetUp]
+        public void SetupAllTests()
+        {
+            p1 = new Product();
+            {
+                p1.ProductCode = "TEST";
+                p1.Description = "Test Product";
+                p1.UnitPrice = 5.00m;
+                p1.OnHandQuantity = 2;
+            }
+
+            p2 = new Product();
+            {
+                p2.ProductCode = "TEST";
+                p2.Description = "Test Product";
+                p2.UnitPrice = 5.00m;
+                p2.OnHandQuantity = 3;
+            }
+        }
+        [Test, Order(1)]
+        public void TestGetProduct()
+        {
+            Product p = ProductDB.GetProduct("VB10");
+            Assert.AreEqual("Murach's Visual Basic 2010", p.Description.Trim());
+        }
+        [Test,Order(2)]
+        public void TestAddProduct()
+        {
+
+            ProductDB.AddProduct(p1);
+            Assert.AreEqual("TEST", p1.ProductCode);
+        }
+        [Test,Order(3)]
+        public void TestUpdateProduct()
+        {
+            ProductDB.UpdateProduct(p1, p2);
+            Assert.AreEqual(3, p2.OnHandQuantity);
+        }
+        [Test, Order(4)]
+        public void TestDeleteProduct()
+        {
+
+            Assert.True(ProductDB.DeleteProduct(p2));
+
+        }
+    }
+}
